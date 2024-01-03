ac76614ef397886c1886863eebff2a2996d09b53
farewell4ever <luzyanin_zakhar@mail.ru>
2024-01-04 00:27:00 +0700

    Добавьте файлы проекта.

---
+++ WpfApp1/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+
+        [Serializable]
+        class Game
+        {
+            public Player Player { get; set; }
+            public Room[] Rooms { get; set; }
+
+            public Game(Player player, Room[] rooms)
+            {
+                Player = player;
+                Rooms = rooms;
+            }
+        }
+}
